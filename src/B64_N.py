3aff63bcab0158864624d396b0b1228ab51d6343
alejo0317 <alejandro.0317@gmail.com>
2016-10-10 10:56:00 -0500

    Replaced kernels

---
+++ src/B64_N.py
@@ -0,0 +1,55 @@
+import pyopencl as cl
+import numpy as np
+from time import time
+from python_functions.utils import memory_usage_psutil
+import psutil, os
+
+
+# Parameters
+k = 40
+s = 100
+
+# Compute output columns
+cSK_10_64  =  (k-1)/32 + 1
+
+# Random data
+total_decimals = (cSK_10_64 * s)
+h_SK_10_64 = np.random.randint(2**63, size=total_decimals).astype("uint64").reshape((s,cSK_10_64))
+
+# Fill first column with valid values
+if (k%32!=0):
+    h_SK_10_64[:,0] =  np.random.randint(2**(k%32), size=s)
+
+# Output matrix
+h_SK_4 = np.ndarray((s, k)).astype(np.uint8)
+
+print "#############  KMERS DECIMAL (64bits) a BASE 4  K = {}, S = {}  #################".format(k, s)
+
+# OpenCL definitions
+contexto = cl.create_some_context()
+cola = cl.CommandQueue(contexto)
+codigo_kernel = open("kernels/B64_N.cl").read()
+programa = cl.Program(contexto, codigo_kernel).build()
+B64_N = programa.B64_N
+B64_N.set_scalar_arg_dtypes([None, None, np.uint32, np.uint32, np.uint32])
+
+# Copy input data from host to device
+d_SK_10_64 = cl.Buffer(contexto, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=h_SK_10_64)
+
+# Output buffer
+d_SK_4 = cl.Buffer(contexto, cl.mem_flags.WRITE_ONLY, h_SK_4.nbytes)
+
+# Execution Range
+rango_global = (k, s)
+
+# Kernel Execution
+print "Executing kernel"
+t1 = time()
+B64_N(cola, rango_global, None, d_SK_4, d_SK_10_64, k, s, cSK_10_64 )
+cola.finish()
+print "Kernel took {} seconds in the execution".format(time()-t1)
+
+# Retrieve output
+cl.enqueue_copy(cola, h_SK_4, d_SK_4)
+
+cola.finish()
---
+++ src/kernels/B32_N.cl
@@ -0,0 +1,19 @@
+__kernel void B32_N(
+   __global uchar* SK_4, // 8 bits (1 nucleotide per cell)
+   __global uint* SK_10_32, // Output matrix
+   const unsigned int k, // K-mer size
+   const unsigned int s, // Number of K-mers
+   const unsigned int cSK_10_32 // Number of K-mers
+   )
+{
+
+   int x = get_global_id(0);
+   int y = get_global_id(1);
+
+
+   // FUNCTION: Splitting
+   if ((x<k) && (y<s)) {
+     SK_4[y*k + x] = SK_10_32[y*cSK_10_32 + ((k-1)/16-(k-x-1)/16)]>>(((k-x-1)%16)*2)&3;
+    }
+
+}
