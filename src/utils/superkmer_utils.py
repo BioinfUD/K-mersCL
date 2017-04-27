c47425b540cc530f1bb6a2a2a484d02a518bcc7d
Cristian Rojas <alejandro.0317@gmail.com>
2017-04-27 00:37:40 -0500

    First attemp for extract superkmers

--- src/utils/superkmer_utils.py
+++ src/utils/superkmer_utils.py
@@ -3,11 +3,20 @@ from read_conversion import int_to_base
 def cut_minimizer_matrix(minimizer_matrix, counter_vector):
     n_reads = counter_vector.shape[0]
     cutted_matrix = []
-    for i in range(n_reads):
+    for i in range(counter_vector.shape[0]):
+        n_superkmers = counter_vector[i][0]
+        cutted_matrix.append(minimizer_matrix[i][0:n_superkmers])
         # Get the values
         pass
     return cutted_matrix
 
 
 def extract_superkmers(reads_matrix, minimizer_matrix):
-    pass
+    superkmers = [] # Composed of tuples (superkmer, minimizer)
+    for read_number, row in minimizer_matrix:
+        for v in row:
+            start =
+            end =
+            minimizer = v >> 20
+            superkmers.append((reads_matrix[read_number][start:end], minimizer)
+    return superkmers
